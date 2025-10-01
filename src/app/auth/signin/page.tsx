e78076ba4519a05d9f481299be6c11b56d1a29b5
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-01 12:40:46 +0200

    signin page created

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,9 +1,46 @@
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Github } from "lucide-react";
+import Link from "next/link";
 
+function SignInPage() {
+    return (
 
-function page() {
-  return (
-    <div>Sign In Page</div>
-  )
+        <div className="flex justify-center items-center min-h-[calc(100vh-20rem)]"> 
+            <Card className="w-full max-w-md border bg-card backdrop-blur">
+            <CardHeader className="text-center">
+                <CardTitle className="text-2xl">Welcome the JobBew</CardTitle>
+                <CardDescription>Sign in to post jobs or apply for opportunites</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <Button
+                    variant="outline"
+                    size="lg"
+                    className="w-full justify-center gap-2" >
+                    <Github className="h-5 w-5" />
+                    Continue with GitHub
+                </Button>
+                <p className="text-center text-xs text-muted-foreground">
+                    By signing in, you agree to our{" "}
+                    <Link href="/terms" className="underline underline-offset-4">Terms of Service</Link>
+                    {" "}and{" "}
+                    <Link href="/privacy" className="underline underline-offset-4">Privacy Policy</Link>.
+                </p>
+
+            </CardContent>
+
+        </Card>
+        </div>
+
+    )
 }
 
-export default page
+export default SignInPage
