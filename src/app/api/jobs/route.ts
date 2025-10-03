279449041cc42876d04e51923e43d7f8f671896d
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-03 17:09:33 +0200

    job posting api created

---
+++ src/app/api/jobs/route.ts
@@ -0,0 +1,30 @@
+import {auth} from "@/auth"
+import { NextResponse } from "next/server";
+import {prisma} from "@/lib/prisma"
+
+export const POST = async(req: Request) => {
+    
+        const session = await auth(); // auth.ts
+        if(!session?.user || !session?.user.id){ //if session.user not exist, redirect to /auth/signin
+            return NextResponse.redirect(new URL("/auth/signin", req.url))
+        }
+
+        try {
+
+            const data = await req.json()// from client application/json
+            const job = await prisma.job.create({
+                data : {
+                    ...data,
+                    postedById : session.user.id
+                }
+            })
+            return NextResponse.json(job)
+            
+        } catch (error) {
+            console.log("Error creating a job ",error)
+            return new NextResponse("Internal server error", {status : 500})
+        }
+     
+        
+    
+}
