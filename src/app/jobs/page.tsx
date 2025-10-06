aed381a763d11f88758cd3b4dd44d1a195b8ed39
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-06 12:41:39 +0200

    find-job functionality

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -9,50 +9,53 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import Link from "next/link";
-
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import { Button } from "@/components/ui/button";
+import FindJob from "@/components/FindJob";
 import { Badge } from "@/components/ui/badge"
 import { MapPin } from 'lucide-react'
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
 
-const page = async () => {
 
-    //const [jobType, setJobType] = useState("")
 
-    const jobs = await prisma.job.findMany({ orderBy: { postedAt: "desc" }, include: { postedBy: true }, });
+export default async function page({
+    searchParams,
+}: {
+    searchParams: Promise<{ q?: string; type?: string; location?: string }>
+}) {
+    const sp = await searchParams; // <-- önemli
+
+    const q = typeof sp.q === "string" ? sp.q : "";
+    const type = typeof sp.type === "string" ? sp.type : "";
+    const location = typeof sp.location === "string" ? sp.location : "";
+    const jobs = await prisma.job.findMany({
+        where: {
+            AND: [
+                q ? {
+                    OR: [
+                        { title: { contains: q, mode: "insensitive" } },
+                        { type: { contains: q, mode: "insensitive" } },
+                        { location: { contains: q, mode: "insensitive" } },
+                    ]
+                } : {},
+                type ? {type : type} : {},
+                location ? {
+                    OR : [
+                        {title : {contains : location, mode : "insensitive"}},
+                        {type : {contains : location, mode : "insensitive"}},
+                        {location : {contains : location, mode : "insensitive"}},
+                    ]
+                } : {}
+
+            ]
+        }, orderBy: { postedAt: "desc" }, include: { postedBy: true },
+    });
 
     return (
         <div className="mx-auto max-w-5xl">
 
-            <Card className="px-4">
-                <h1 className="text-2xl font-bold">Find a Job</h1>
-                <form className="grid grid-cols-1 md:grid-cols-3 gap-8 mx-6 mt-6">
-
-                    <Input placeholder="Search jobb.." />
-                    <select name="type" id="type" className="w-full h-10 rounded-md border border-input bg-black-500 px-3 text-sm
-                        focus:outline-none focus:ring-2 focus:ring-ring
-                        [appearance:none] pr-8 relative">
-                        <option value="">All type</option>
-                        <option value="full-time">Full-time</option>
-                        <option value="part-time">Part-time</option>
-                        <option value="freelance">Freelance</option>
-                        <option value="internship">Internship</option>
-                        <option value="volunteer">Volunteer</option>
-                    </select>
-                    <Input placeholder="location" />
-                    <Button className="col-span-full" type="submit" variant="outline">Search</Button>
-                </form>
-            </Card >
+            <FindJob
+                initialQ={q}
+                initialType={type}
+                initialLocation={location}
+            />
 
             <div className="grid grid-cols-1 md:grid-cols-2 md:gap-6 gap-8  mt-6">
                 {jobs.map((job, key) => (
@@ -72,12 +75,12 @@ const page = async () => {
                         <CardContent className="text-left">
                             <p>{job.description}</p>
                         </CardContent>
-                        <CardFooter className="mt-auto flex items-center justify-between">
+                        <CardFooter className="mt-auto flex items-center justify-between ">
                             <div>
-                                <Badge className="text-sm lg:text-md" variant="outline">Posted by {job.postedBy.name}</Badge>
-                                <Badge className="text-sm lg:text-md" variant="outline">{job.type}</Badge>
+                                <Badge className="text-sm lg:text-md mx-0.5 my-0.5" variant="outline">Posted by {job.postedBy.name}</Badge>
+                                <Badge className="text-sm lg:text-md mx-0.5 my-0.5" variant="outline">{job.type}</Badge>
                             </div>
-                            <Link href={`jobs/${job.id}`} className="text-indigo-500 hover:text-indigo-800 font-medium">View Details </Link>
+                            <Link href={`jobs/${job.id}`} className="text-indigo-500 hover:text-indigo-800 font-medium text-sm md:text-md">View Details</Link>
                         </CardFooter>
 
 
@@ -91,5 +94,5 @@ const page = async () => {
     )
 }
 
-export default page;
+
 
