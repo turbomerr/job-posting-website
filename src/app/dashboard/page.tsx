e07cd1b14c3b86b239ae63134a9aea109776bab6
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-08 16:29:15 +0200

    dashboard fetch completed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,43 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import { signOut } from "next-auth/react"
+import { auth } from "@/auth"
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/prisma"
 
-function page() {
-    
-  return (
-    <div>
-        <h1>dashboard</h1>
-        <Button variant="ghost" onClick={() => signOut()}>Logout</Button>
-    </div>
-  )
-}
+import DashboardCard from "@/components/DashboardCard";
 
-export default page
+
+export default async function page() {
+
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    redirect("/auth/signin")
+  }
+
+  const [applications, postedJobs] = await Promise.all([
+    // Applications query
+    prisma.application.findMany({
+      where: {
+        userId: session.user.id,
+      },
+      include: {
+        job: {
+          include: {
+            postedBy: true,
+          },
+        },
+      },
+      orderBy: {
+        appliedAt: "desc",
+      },
+    }),
+    //job query
+    prisma.job.findMany({
+      where: { postedById: session.user.id },
+      include: { _count: { select: { applications: true } } },
+      orderBy: { postedAt: "desc" }
+    })
+  ])
+
+
+  return ( <DashboardCard applications={applications} postedJobs={postedJobs}/> );}
+
